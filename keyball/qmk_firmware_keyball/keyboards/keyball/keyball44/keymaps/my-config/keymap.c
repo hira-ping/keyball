d6cb9786942172ebaab8548062b1a51e36798175
“hira-ping” <233596449+hira-ping@users.noreply.github.com>
2025-10-06 13:42:17 +0000

    feat(keymap): Migrate keymap to Keyball44 layout

---
+++ keyball/qmk_firmware_keyball/keyboards/keyball/keyball44/keymaps/my-config/keymap.c
@@ -0,0 +1,125 @@
+/*
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+#include "quantum.h"
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // 0: 
+  [0] = LAYOUT_universal(
+    KC_NO,  MT(MOD_RCTL, KC_Q),    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_NO,
+    KC_NO,             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_BTN1,   KC_NO,
+    MO(1),             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                           KC_N,    KC_M,    MT(MOD_LCTL, KC_COMM), MT(MOD_RGUI,KC_DOT), MT(MOD_RSFT, KC_BTN2), KC_NO,
+                      KC_ESC,  KC_LALT, MT(MOD_LGUI,KC_BSPC), LT(2,KC_SPC), MT(MOD_LCTL,KC_TAB) ,                       LT(2, KC_LNG2), KC_ENT, MT(MOD_LSFT,KC_TAB), _______,   MO(3)
+  ),
+
+  // 1: 
+  [1] = LAYOUT_universal(
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                            KC_PMNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_NO,     KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                            KC_KP_0, KC_KP_4, KC_KP_5, KC_KP_6, KC_TRNS,   KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                            KC_PDOT, KC_KP_1, KC_KP_2, KC_KP_3, KC_TRNS,   KC_NO,
+                      KC_NO,   KC_NO,   _______,          _______,          _______,              _______, KC_TRNS, KC_TRNS, _______,   _______
+  ),
+
+  // 2: 
+  [2] = LAYOUT_universal(
+    KC_NO, KC_RCTL, KC_LBRC, KC_RBRC, KC_SCLN, S(KC_SCLN),                                       KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,     KC_NO,
+    KC_NO, KC_RCMD, S(KC_9), S(KC_0), KC_BSLS, S(KC_BSLS),                                       KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,   KC_NO,
+    KC_NO, KC_RSFT, S(KC_LBRC), S(KC_RBRC), S(KC_QUOT), KC_QUOT,                                 KC_NO, C(S(KC_TAB)), KC_NO, C(KC_TAB), KC_TRNS,   KC_NO,
+                    S(KC_COMM), S(KC_DOT),KC_NO , _______,          _______,                   _______, KC_TRNS, KC_TRNS, _______,   _______
+  ),
+
+  // 3: 
+  [3] = LAYOUT_universal(
+    S(KC_5), S(KC_5), S(KC_3), S(KC_6), S(KC_8), S(KC_EQL),                                        _______, KC_WH_U, C(KC_UP), KC_NO,   KC_NO,     KC_NO,
+    S(KC_2), S(KC_2), S(KC_4), KC_MINS, KC_SLSH, KC_EQL,                                           KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,   KC_NO,
+    S(KC_GRV), S(KC_GRV), S(KC_7), S(KC_MINS), S(KC_SLSH), S(KC_1),                                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,
+                      KC_NO,   KC_GRV,  _______,          _______,          _______,              _______, KC_LNG1, _______, _______,   _______
+  ),
+};
+// clang-format on
+
+
+#ifdef OLED_ENABLE
+
+#    include "lib/oledkit/oledkit.h"
+
+void oledkit_render_info_user(void) {
+    keyball_oled_render_keyinfo();
+    keyball_oled_render_ballinfo();
+    keyball_oled_render_layerinfo();
+}
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Enable auto-scroll mode on Layer 3
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+
+    // First, always ensure the RGB light system is enabled
+    rgblight_enable_noeeprom();
+
+    switch (get_highest_layer(state)) {
+        case 0: // Base layer
+            // Turn off the light by setting brightness (value) to 0
+            rgblight_sethsv_noeeprom(0, 0, 0);
+            break;
+        case 1: // Layer 1
+            rgblight_sethsv_noeeprom(HSV_BLUE);
+            break;
+        case 2: // Layer 2
+            rgblight_sethsv_noeeprom(HSV_GREEN);
+            break;
+        case 3: // Layer 3 (Scroll Mode)
+            rgblight_sethsv_noeeprom(HSV_GOLD);
+            break;
+        case 4: // Shift+Cmd Mode Layer
+            // Enable modifier keys on this layer
+            add_mods(MOD_LSFT | MOD_LGUI);
+            rgblight_sethsv_noeeprom(HSV_MAGENTA);
+            break;
+    }
+    return state;
+}
+#endif
+
+// This function runs on every keyboard matrix scan.
+void matrix_scan_user(void) {
+  // This line calls led_update_user and passes the current LED state.
+  led_update_user(host_keyboard_led_state()); 
+}
+
+bool led_update_user(led_t led_state) {
+    uint8_t mods = get_mods();
+
+    if (mods & MOD_MASK_SHIFT) { // while Shift is held
+        rgblight_sethsv_noeeprom(HSV_CYAN);
+
+    } else if (mods & MOD_MASK_CTRL) { // while Ctrl is held
+        rgblight_sethsv_noeeprom(HSV_RED);
+
+    } else if (mods & MOD_MASK_GUI) { // Command/Win key
+        rgblight_sethsv_noeeprom(HSV_PURPLE);
+    } else {
+        // If no modifier keys are pressed, revert to the layer color.
+        layer_state_set_user(layer_state);
+    }
+    return true;
+}
